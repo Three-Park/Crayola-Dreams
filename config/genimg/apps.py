6d09efd2ddb71ddf0d2cb493905f762ab8f6ad58
Min Gyung Park <kateking001130@ewhain.net>
2024-01-11 17:57:51 +0900

    [UPDATE]genimg apps.py

--- config/genimg/apps.py
+++ config/genimg/apps.py
@@ -10,9 +10,9 @@ class GenimgConfig(AppConfig):
     name = 'genimg'
 
     base_text = open("./genimg/genTextBase.txt", 'r', encoding='utf-8').readlines()
-    openai.api_key="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"
+    openai.api_key=""# API Key
     stability_api = client.StabilityInference(
-        key="sk-VTJ5TddSQAqi7MOjNeyw1oPfrsMK9xkD1Iz0Bpmd2cd8HIF3",# API Key reference.
+        key="",# API Key reference.
         verbose=True, 
         engine="stable-diffusion-xl-1024-v1-0"
         )
