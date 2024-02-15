822a3b00f714fb6309734485bc2010c7181184bd
PYS_73 <128453499+zbxlYS@users.noreply.github.com>
2024-02-15 15:33:04 +0900

    Add files via upload

--- Crayola-Dreams--/Fairy-Taiary/config/diary/urls.py
+++ Crayola-Dreams--/Fairy-Taiary/config/diary/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from .views import create_diary, diary_list, view_diary, edit_diary, delete_diary,image_select
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import create_diary, diary_list, view_diary, edit_diary, delete_diary, image_select
 
 urlpatterns = [
     path('create/', create_diary, name='create_diary'),
@@ -10,5 +10,4 @@ urlpatterns = [
     path('edit/<int:pk>/', edit_diary, name='edit_diary'),
     path('delete/<int:pk>/', delete_diary, name='delete_diary'),
     path('imageselect/<int:pk>/',image_select,name='image_select'),
-
-]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
