5e725c5571cc97d68328a629cfd500b18e66f602
Min Gyung Park <kateking001130@ewhain.net>
2024-04-03 18:45:23 +0900

    Update following

--- fairy_tairy/fairy_tairy/permissions.py
+++ fairy_tairy/fairy_tairy/permissions.py
@@ -9,3 +9,17 @@ class IsOwner(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+    
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    객체를 만든 사용자만 수정할 수 있는 권한.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # 읽기 권한은 항상 허용함
+        if request.method in ['GET', 'HEAD', 'OPTIONS']:
+            return True
+
+        # 요청한 사용자가 해당 객체의 소유자인 경우에만 쓰기 권한을 부여함
+        return obj.follower == request.user or obj.following_user == request.user
