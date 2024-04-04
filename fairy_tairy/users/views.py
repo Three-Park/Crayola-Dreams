08b0980785ecd4a1e47cfae183485facebc71e32
Min Gyung Park <kateking001130@ewhain.net>
2024-04-04 13:25:53 +0900

    Update Following logic

--- fairy_tairy/users/views.py
+++ fairy_tairy/users/views.py
@@ -28,21 +28,28 @@ class FollowViewSet(GenericViewSet,
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        
+        user = self.request.user
         followee = serializer.validated_data.get('following_user')
-        if followee==request.user:
+        if followee==user:
             return Response({"message": f"Cannot Follow yourself, {followee.username}."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if Follow.objects.filter(follower=user, following_user=followee).exists() | Follow.objects.filter(follower=user, following_user=followee).exists():
+            return Response({"message": f"Follow request already sent to {followee.username}."}, status=status.HTTP_400_BAD_REQUEST)
             
-        follow_request, created = Follow.objects.get_or_create(follower=request.user, following_user=followee)
-
+       
+        follow_request, created = Follow.objects.get_or_create(follower=request.user, following_user=followee, status=Follow.REQUESTED)
         if created:
             return Response({"message": f"Follow request sent to {followee.username}"}, status=status.HTTP_201_CREATED)
         else:
             return Response({"message": f"Follow request already sent to {followee.username}"}, status=status.HTTP_400_BAD_REQUEST)
 
+
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({"message": "Follow request deleted"}, status=status.HTTP_204_NO_CONTENT)
+
 
     def update(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -50,11 +57,13 @@ class FollowViewSet(GenericViewSet,
         instance.save()
         return Response({"message": "Follow request accepted"}, status=status.HTTP_200_OK)
 
+
     def partial_update(self, request, *args, **kwargs):
         instance = self.get_object()
         instance.status = Follow.REJECTED
         instance.save()
         return Response({"message": "Follow request rejected"}, status=status.HTTP_200_OK)
+       
         
 # class UserViewSet(viewsets.ModelViewSet):
 #     queryset = User.objects.all()
