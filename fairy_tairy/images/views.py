a3e124998f143f928019dad5a99c21b652a61c03
Min Gyung Park <kateking001130@ewhain.net>
2024-04-12 13:58:35 +0900

    [update] image

--- fairy_tairy/images/views.py
+++ fairy_tairy/images/views.py
@@ -1,65 +1,7 @@
-# from django.shortcuts import render, redirect
-# from django.http import HttpResponse
-# import boto3
-# import urllib.parse
-# from .forms import ImageForm
-# from botocore.exceptions import NoCredentialsError
-# import fairy_tairy.settings as settingss
-# from ai.generate_image import *
-
-# def upload_image(request):
-#     if request.method == 'POST':
-#         form = ImageForm(request.POST, request.FILES)
-#         if form.is_valid():
-#             # 이미지를 S3에 업로드
-#             image = form.cleaned_data['image']
-#             upload_to_s3(image, settingss.AWS_STORAGE_BUCKET_NAME,'images/')
-
-#             # 폼을 저장하여 모델에 이미지 정보 추가
-#             image_instance = form.save(commit=False)
-            
-#             # S3에 업로드된 이미지의 URL 생성
-#             file_name = urllib.parse.quote(image.name)
-#             image_instance.image_url = f"https://{settingss.AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/images/{file_name}"
-            
-#             image_instance.save()
-
-#             return HttpResponse('Upload Successful')
-
-#     else:
-#         form = ImageForm()
-
-#     return render(request, 'upload_image.html', {'form': form})
-
-# def upload_to_s3(encoded_image):
-#     s3_client = boto3.client('s3', aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-#                       aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-
-#     try:
-#         image_data = base64.b64decode(encoded_image)
-#         # image = Image.open(io.BytesIO(image_data))
-#         image_key = 'images/{}.png'.format(uuid.uuid4())  # 이미지 파일명을 랜덤하게 생성
-        
-#         # 이미지를 S3에 업로드
-#         s3_client.put_object(Body=image_data, Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=image_key)
-#         image_url = 'https://{}.s3.amazonaws.com/{}'.format(settings.AWS_STORAGE_BUCKET_NAME, image_key)
-        
-#         return image_url
-    
-#     except Exception as e:
-#         print("Exception occurred while uploading image to S3:", e)
-#         return None
-    
-"""--------------아래 drf를 이용하는 코드로 바꿀 예정---------------------"""
 from rest_framework import mixins,status
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.response import Response
-from django.core.files.base import ContentFile
-from django.core.files.uploadedfile import InMemoryUploadedFile
-from botocore.exceptions import ClientError
-from django.http import JsonResponse
-
 
 from .serializers import *
 from .models import *
@@ -72,8 +14,6 @@ import base64
 import boto3
 import uuid
 import time
-import io
-import os
 
 def request_image_from_flask(prompt):
     # Flask 서버의 URL
@@ -128,9 +68,20 @@ class ImageViewSet(GenericViewSet,
             if not image_url:
                 return Response({'error': "Failed to get image from Flask"}, status=status.HTTP_400_BAD_REQUEST)
                 
-            print('save')
-            new_image = Image.objects.get_or_create(diary=diary, image_url=f"{image_url}.png", image_prompt=image_prompt)
-            return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
+            existing_image = Image.objects.filter(diary=diary).first()
+                
+            if existing_image:
+                # 이미지가 존재하면 해당 이미지를 수정
+                print('ex')
+                existing_image.image = image_url
+                existing_image.save()
+                return Response({"message": "Image updated successfully"}, status=status.HTTP_200_OK)
+                
+            else:
+                # 이미지가 존재하지 않으면 새로운 이미지를 생성
+                print('save')
+                new_image = Image.objects.get_or_create(diary=diary, image_url=image_url, image_prompt=image_prompt)
+                return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({'error': f"Error uploading image: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
@@ -181,13 +132,24 @@ class ImageAdminViewSet(GenericViewSet,
             print('img: ',image_url)
             if not image_url:
                 return Response({'error': "Failed to get image from Flask"}, status=status.HTTP_400_BAD_REQUEST)
-
-            new_image = Image.objects.get_or_create(diary=diary, image_url=f"{image_url}.png", image_prompt=image_prompt)
-            return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
+                
+            existing_image = Image.objects.filter(diary=diary).first()
+                
+            if existing_image:
+                # 이미지가 존재하면 해당 이미지를 수정
+                print('ex')
+                existing_image.image = image_url
+                existing_image.save()
+                return Response({"message": "Image updated successfully"}, status=status.HTTP_200_OK)
+                
+            else:
+                # 이미지가 존재하지 않으면 새로운 이미지를 생성
+                print('save')
+                new_image = Image.objects.get_or_create(diary=diary, image_url=image_url, image_prompt=image_prompt)
+                return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
 
         except Exception as e:
             return Response({'error': f"Error uploading image: {str(e)}"}, status=status.HTTP_400_BAD_REQUEST)
-        
         
         # 이미지 목록 조회 기능
     def list(self, request, *args, **kwargs):
